dd3e652a861da249e8054f77d3a7c1666bbe7b37
Dmitry Svyatov <dimka201993@gmail.com>
2022-07-13 11:11:25 +0300

    Задание 41 пустых строк нет)

---
+++ 41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("В введите последовательность чисел , разделяя каждое число запятой ");
+string numbers =Console.ReadLine(); 
+Console.WriteLine(numbers);
+int count=0;
+if (numbers[0] != '-'&& numbers[0]!='0')
+    {
+        count++;
+    }
+for(int i=0;i<numbers.Length;i++)
+{
+    if (numbers[i] == ',' && numbers[i + 1] != '-' && numbers[i + 1] != '0')
+    {
+        count++;
+    }   
+}
+Console.WriteLine("В веденом диапазоне  "+ count + " чисел больше 0");
